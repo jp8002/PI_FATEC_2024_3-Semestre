20849cde6feac3e11f0192e680185ac90cb78cf8
KauePO <161944951+KauePO@users.noreply.github.com>
2025-04-27 21:19:34 -0300

    Refatoração do Backend da agenda do cliente

--- studio/core/services.py
+++ studio/core/services.py
@@ -1,5 +1,6 @@
 import pymongo
 from bson.objectid  import ObjectId
+from datetime import datetime
 
 class Autenticar:
     def AuthSession(cookies):
@@ -38,18 +39,14 @@ class ServiceMongo:
         return cliente
     
     def consultar_datas_agendadas(self): # O MÉTODO ATUALMENTE RETORNA TODAS AS DATAS (BASEADAS NAS ASSINATURAS ATIVAS) NO FORMATO DD/MM/AAAA
-        client = pymongo.MongoClient('mongodb://localhost:27017')
-        mydb = client["studio"]
-        colecao = mydb["clientes"]
-        
         datas_agendadas = []
         
         query = {"status":"ativo"}
-        for cadastro in colecao.find(query):
+        for cadastro in self._colecao.find(query):
             if "data_assinatura" in cadastro:
                 data_str = cadastro["data_assinatura"]
                 data_datetime = datetime.strptime(data_str, "%Y-%m-%dT%H:%M:%S")
                 data = data_datetime.strftime("%d/%m/%Y")
                 datas_agendadas.append(data)
 
-        
+        return datas_agendadas
--- studio/core/urls.py
+++ studio/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.View_Pagina_Inicial, name="paginaInicial")
+    path('', views.View_Pagina_Inicial, name="paginaInicial"),
+    path('calendario/', views.Calendario, name="paginaCalendario")
+
 ]
