7ae9f2212586ec182c2d6b0d5894c61399797904
jp8002 <162762832+jp8002@users.noreply.github.com>
2025-06-25 17:01:00 -0300

    .

--- studio/core/tests/testeViewDashboard.py
+++ studio/core/tests/testeViewDashboard.py
@@ -32,7 +32,7 @@ class TestDashboardViewGET(TestCase):
         # Criar personal admin real no banco (acesso = "adm")
         self.mongo = ConexaoMongo()
 
-        self.mongo._colecao = self.mongo._mydb['personal']
+        self.mongo._colecao = self.mongo.mydb['personal']
         self.mongo._colecao.insert_one({"nome": "Joana Costa", "senha": "joana123", "telefone": "(11) 91234-0001", "email": "joana.costa@academia.com", "salario": 3000, "cpf": "12333678910", "acesso": "adm", "cref": "123456-G/SP"})
 
 
